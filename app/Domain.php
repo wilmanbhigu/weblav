5e5041137cfc3fd40075ca66c6f7b1702498ed83
wilmanhigu <wngeybhigu@gmail.com>
2018-06-07 17:55:43 +0800

    resolved merge conflict

---
+++ app/Domain.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Domain extends Model
+{
+    //
+    protected $table = 'tbl_domain';
+    public $timestamps = false;
+
+    
+}
---
+++ app/Instansi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Instansi extends Model
+{
+	// mendefinisikan tabel
+	protected $table='instansi';
+
+	// mengabaikan timestamps
+	public $timestamps = false;
+    
+}
